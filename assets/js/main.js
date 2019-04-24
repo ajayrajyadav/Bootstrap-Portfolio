e77821892cda8c3747a946f74af683232a98357a
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-04-24 15:01:11 -0700

    Adding acoridan style to portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+//     $(document).on("click", ".collapsible", function(){
+//         collapsible();
+//     })
+//     // $('.collapsible').collapsible();
+// });
+$(document).ready(function(){
+    // $('.collapsible').collapsible();
+  });
+
+  function collapsible() {
+      console.log("collapsing");
+      
+      
+  }
+      
